db4c966d7eb03c7cbdd7b8e843ae1f61d8cbdb93
Chanudhi <aluchanu29@gmail.com>
2025-02-11 12:50:42 +0530

    commit

--- timeless/lib/main.dart
+++ timeless/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'screens/login_screen.dart';
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
